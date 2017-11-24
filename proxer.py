61e1f76b9858ed53dac486229f3b495c7765f7a6
bargst <bargst@wasabout.net>
2017-11-24 17:46:07 +0100

    Add commandline options

--- proxer.py
+++ proxer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import requests
+import argparse
+
+from web3 import HTTPProvider, IPCProvider
+
 import gevent
 import gevent.wsgi
 import gevent.queue
@@ -10,16 +15,18 @@ from tinyrpc.transports.wsgi import WsgiServerTransport
 from tinyrpc.server.gevent import RPCServerGreenlets
 from tinyrpc.dispatch import RPCDispatcher
 
-from web3 import HTTPProvider
-#provider = HTTPProvider('http://localhost:8545')
-provider = HTTPProvider('https://mainnet.infura.io')
-
 class ProxyDispatcher(RPCDispatcher):
+
+    def __init__(self, providers, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.providers = providers
 
     def get_method(self, name):
 
+        print(self.providers)
         def provider_method(*args):
-            return provider.make_request(name, params=args)
+            for provider in self.providers:
+                return provider.make_request(name, params=args)
 
         try:
             return super().get_method(name)
@@ -28,32 +35,50 @@ class ProxyDispatcher(RPCDispatcher):
 
         raise KeyError(name)
 
-dispatcher = ProxyDispatcher()
-transport = WsgiServerTransport(queue_class=gevent.queue.Queue)
 
-# start wsgi server as a background-greenlet
-wsgi_server = gevent.wsgi.WSGIServer(('127.0.0.1', 8545), transport.handle)
-gevent.spawn(wsgi_server.serve_forever)
+class Proxer:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--ipc", help="Add IPC-RPC provider", action='append_const', dest='providers', const=IPCProvider())
+        parser.add_argument("--local-rpc", help="Add HTTP-RPC provider: http://localhost:8545", action='append_const', dest='providers', const=HTTPProvider('http://localhost:8545'))
+        parser.add_argument("--rpc", help="Add HTTP-RPC provider", metavar='http://host:port', action='append', dest='providers', type=HTTPProvider)
+        parser.add_argument("--manager", help="Account manager acces URL", metavar='http://host:port', type=str)
+        parser.add_argument("--debug", help="Enable debug output", dest='debug', action='store_true')
+        parser.add_argument("--trace", help="Enable trace output", dest='trace', action='store_true')
+        self.args = parser.parse_args()
+        
+        self.providers = self.args.providers
+        self.transport = WsgiServerTransport(queue_class=gevent.queue.Queue)
+        self.wsgi_server = gevent.wsgi.WSGIServer(('127.0.0.1', 8545), self.transport.handle)
+        self.dispatcher = ProxyDispatcher(self.providers)
+        self.rpc_server = RPCServerGreenlets(
+            self.transport,
+            JSONRPCProtocol(),
+            self.dispatcher
+        )
 
-rpc_server = RPCServerGreenlets(
-    transport,
-    JSONRPCProtocol(),
-    dispatcher
-)
 
-import requests
-account_url = 'http://localhost:5000/account/'
+    def start(self):
+        # start wsgi server as a background-greenlet
+        gevent.spawn(self.wsgi_server.serve_forever)
+        
+        # in the main greenlet, run our rpc_server
+        self.rpc_server.serve_forever()
 
-@dispatcher.public
-def eth_sign(addr, data):
-    return requests.put(account_url + addr, json={'hexstr':data}).json()
+#account_url = 'http://localhost:5000/account/'
+#account_url = self.args.account_manager
 
-@dispatcher.public
-def eth_sendTransaction(transaction):
-    addr = transaction.pop('from')
-    #TODO check transaction content
-    signed = requests.put(account_url + addr, json=transaction).json()
-    return dispatcher.get_method('eth_sendRawTransaction')(signed)
+#    @dispatcher.public
+#    def eth_sign(addr, data):
+#        return requests.put(account_url + addr, json={'hexstr':data}).json()
 
-# in the main greenlet, run our rpc_server
-rpc_server.serve_forever()
+#    @dispatcher.public
+#    def eth_sendTransaction(transaction):
+#        addr = transaction.pop('from')
+#        #TODO check transaction content
+#        signed = requests.put(account_url + addr, json=transaction).json()
+#        return dispatcher.get_method('eth_sendRawTransaction')(signed)
+
+
+if __name__ == '__main__':
+    Proxer().start()
