94b5b14df8f5773cb79c4ead471a31d91e8cd388
bargst <bargst@wasabout.net>
2017-11-28 13:47:28 +0100

    Update proxer test

--- proxer.t.py
+++ proxer.t.py
@@ -4,30 +4,27 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--rpc", help="A remote HTTP-RPC ethereum node", metavar='http://host:port', type=str, default='http://localhost:8545')
+parser.add_argument("--proxer-rpc", help="A remote HTTP-RPC ethereum node", metavar='http://host:port', type=str, default='http://localhost:8545')
 parser.add_argument("--remote-rpc", help="A remote HTTP-RPC ethereum node", metavar='http://host:port', type=str)
 parser.add_argument("--tinyrpc", help="Test using tinyrpc client", action='store_true', default=False)
 parser.add_argument("--no-web3", help="Test not using web3", action='store_true', default=False)
 parser.add_argument("--account", help="Use account to test signing")
 args = parser.parse_args()
 
+# web3 tests
 if not args.no_web3:
     from web3 import Web3, HTTPProvider, IPCProvider
 
-    proxer_web3 = Web3(HTTPProvider(args.rpc))
-    print('Proxer eth_syncing: {}'.format(proxer_web3.eth.syncing))
-    print('Proxer eth_blockNumber: {}'.format(proxer_web3.eth.blockNumber))
-    if args.account:
-        print(args.account)
-        print('Proxer eth_sign: {}'.format(proxer_web3.eth.sign(args.account, text='test')))
-
+    providers = { 'Proxer' : Web3(HTTPProvider(args.proxer_rpc)) }
     if args.remote_rpc:
-        direct_web3 = Web3(HTTPProvider(args.remote_rpc))
-        print('Direct eth_syncing: {}'.format(direct_web3.eth.syncing))
-        print('Direct eth_blockNumber: {}'.format(direct_web3.eth.blockNumber))
+        providers['Remote'] = Web3(HTTPProvider(args.remote_rpc))
 
+    for provider in providers:
+        print('{} eth_syncing: {}'.format(provider, providers[provider].eth.syncing))
+        print('{} eth_blockNumber: {}'.format(provider, providers[provider].eth.blockNumber))
+        print('{} eth_getCode: {}'.format(provider, providers[provider].eth.getCode('0xe819300B6f3d0625632B47196233fe6671a59891')))
         if args.account:
-            print('Direct eth_sign: {}'.format(direct_web3.eth.sign(args.account, text='test')))
+            print('{} eth_sign: {}'.format(provider, providers[provider].eth.sign(args.account, text='test')))
 
 # tinyrpc tests
 if args.tinyrpc:
@@ -38,16 +35,16 @@ if args.tinyrpc:
     # tinyrpc client setup
     rpc_client = RPCClient(
         JSONRPCProtocol(),
-        HttpPostClientTransport(args.rpc)
+        HttpPostClientTransport(args.proxer_rpc)
     )
     remote_server = rpc_client.get_proxy()
 
-    print('eth_blockNumber: {}'.format(remote_server.eth_blockNumber()))
-    print('eth_syncing    : {}'.format(remote_server.eth_syncing()))
+    print('Proxer(tinyrpc) eth_syncing    : {}'.format(remote_server.eth_syncing()))
+    print('Proxer(tinyrpc) eth_blockNumber: {}'.format(remote_server.eth_blockNumber()))
 
     if args.account:
         print(args.account)
-        print('eth_sign: {}'.format(remote_server.eth_sign(args.account, '0x74657376')))
+        print('Proxer(tinyrpc) eth_sign: {}'.format(remote_server.eth_sign(args.account, '0x74657376')))
         transaction = {'from': args.account,
                        'to': '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
                        'value': 0,
@@ -57,4 +54,4 @@ if args.tinyrpc:
                        'chainId': 1,
                        'data': '',
                        }
-        print('eth_sendTransaction: {}'.format(remote_server.eth_sendTransaction(transaction)))
+        print('Proxer(tinyrpc) eth_sendTransaction: {}'.format(remote_server.eth_sendTransaction(transaction)))
