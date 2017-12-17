15d5fc0605d26581b5ad5dd8fb0f6565696ea696
bargst <bargst@wasabout.net>
2017-12-17 20:28:56 +0100

    Fix oasis book fetching

--- markets/oasis.py
+++ markets/oasis.py
@@ -13,40 +13,36 @@ class API(object):
         self.depth = {}
         self.depth['bids'] = []
         self.depth['asks'] = []
-        self.from_block = ''
 
     def update_depth(self, order):
-        # Remove oid from depth before, re-add it if active
-        oid = order.order_id
-        asks = [ order for order in self.depth['asks'] if order['id'] != oid]
-        bids = [ order for order in self.depth['bids'] if order['id'] != oid]
-
         sell_amount = order.pay_amount
         sell_token = order.pay_token
         buy_amount = order.buy_amount
         buy_token  = order.buy_token
+        oid = order.order_id
 
         # price = quote/base
         if sell_token == self.base and buy_token == self.quote:
-            asks.append({
+            self.depth['asks'].append({
                 'price': float(order.buy_to_sell_price),
                 'amount': float(sell_amount),
                 'id': oid,
                 })
         if sell_token == self.quote and buy_token == self.base:
-            bids.append({
+            self.depth['bids'].append({
                 'price': float(order.sell_to_buy_price),
                 'amount': float(sell_amount),
                 'id': oid,
                 })
 
-        self.depth['asks'] = asks
-        self.depth['bids'] = bids
         self.depth['asks'].sort(key=lambda order: order['price'])
         self.depth['bids'].sort(key=lambda order: order['price'])
 
     def get_book(self):
         #TODO use filter to update book
+        self.depth = {}
+        self.depth['bids'] = []
+        self.depth['asks'] = []
         for order in self.oasis.get_orders():
             self.update_depth(order)
         return self.depth
